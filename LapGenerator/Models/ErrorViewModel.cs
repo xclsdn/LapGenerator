4e30d886a873856dcbd01ad53eb24cd6d59f7669
Shizhong He <xclsdn@163.com>
2022-04-04 14:50:01 -0600

    Add project files.

---
+++ LapGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LapGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
